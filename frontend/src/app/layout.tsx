64d3291737bfe2f67be8145560c308462c46d4e6
Agreste Zeladoria <agreste@zeladoria.com.br>
2026-02-03 15:01:04 -0300

    debug: desabilitar TODOS os scripts do template temporariamente
    Teste final: se o loading parar, algum script do template Acorn
    (popper, simplebar, bootstrap, feather) está causando o problema.
    
    Se continuar, o problema está no nosso código React.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -33,12 +33,12 @@ export default function RootLayout({
         <AuthProvider>
           {children}
         </AuthProvider>
-        <Script src="/assets/js/plugins/popper.min.js" strategy="beforeInteractive" />
-        <Script src="/assets/js/plugins/simplebar.min.js" strategy="beforeInteractive" />
-        <Script src="/assets/js/plugins/bootstrap.min.js" strategy="beforeInteractive" />
-        {/* TEMPORARIAMENTE DESABILITADO - estava causando loop infinito */}
+        {/* TODOS OS SCRIPTS DESABILITADOS TEMPORARIAMENTE PARA DEBUG */}
+        {/* <Script src="/assets/js/plugins/popper.min.js" strategy="beforeInteractive" /> */}
+        {/* <Script src="/assets/js/plugins/simplebar.min.js" strategy="beforeInteractive" /> */}
+        {/* <Script src="/assets/js/plugins/bootstrap.min.js" strategy="beforeInteractive" /> */}
         {/* <Script src="/assets/js/fonts/custom-font.js" strategy="afterInteractive" /> */}
-        <Script src="/assets/js/plugins/feather.min.js" strategy="afterInteractive" />
+        {/* <Script src="/assets/js/plugins/feather.min.js" strategy="afterInteractive" /> */}
       </body>
     </html>
   );
